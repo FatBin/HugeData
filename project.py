37eaca0c74d76187aaeeb46ff8955c7c4c45fa00
Peifu Yu <peifuyu@10-18-86-54.dynapool.wireless.nyu.edu>
2019-11-23 13:58:18 -0500

    final template

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import sys
 import pyspark
 import string
-from csv import reader
+import json
 
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
@@ -21,25 +21,105 @@ if __name__ == "__main__":
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
     #test code
-    # name = fileNames[10]
+    # name = fileNames[5]
     # filePath = directory + "/" + name +".tsv.gz"
     # fileDF = spark.read.format('csv').options(header='true', inferschema='true', delimiter='\t').load(filePath)
-    # print(str(fileDF.dtypes))
+    # outputDicts = {}
+    # noEmptyDF = fileDF.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in fileDF.columns])
+    # outputDicts["columns"] = []
+    # for c in fileDF.columns:
+    #     pdict = {
+    #         "column_name": c
+    #     }
+    #     nonEmptyCell = noEmptyDF.select(c).first()[c]
+    #     pdict["number_non_empty_cells"] = int(nonEmptyCell)
+    #     outputDicts["columns"].append(pdict)
+    # outString = str(outputDicts)
 
+    finalOut = []
+    cnt = 0
+    fNum = len(fileNames)
     for name in fileNames:
+        outputDicts = {}
+        cnt += 1
+        print('*'*20)
+        print('{}/{}'.format(cnt, fNum))
+        outputDicts["dataset_name"] = name
         filePath = directory + "/" + name +".tsv.gz"
         fileDF = spark.read.format('csv').options(header='true', inferschema='true', delimiter='\t').load(filePath)
-        #fileDF.coalesce(1).write.format('json').save('test.json')
+        outputDicts = {}
+        outputDicts["columns"] = []
         #1 non empty cell
-        noEmptyDF = fileDF.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in fileDF.columns]).show()
+        print('*'*20)
+        noEmptyDF = fileDF.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in fileDF.columns])
+        print('finished 1')
         #2 empty cell
-        emptyDF = fileDF.select([count(when(not isnan(c) and not col(c).isNull(), c)).alias(c) for c in fileDF.columns]).show()
+        print('*'*20)
+        emptyDF = fileDF.select([count(when(~isnan(c) & ~col(c).isNull(), c)).alias(c) for c in fileDF.columns])
+        print('finished 2')
         #3 distinct cell
-        disNumDF = fileDF.agg(*(F.countDistinct(col(c)).alias(c) for c in fileDF.columns))
+        print('*'*20)
+        disNumDF = fileDF.select([F.countDistinct(col(c)).alias(c) for c in fileDF.columns])
+        print('finished 3')
         #4 most frequent top 5
-        
-    #5 Data types
-    fileDF.dtypes
 
+        ## add to output json
+        outputDicts["columns"] = []
+        colCnt = 0
+        colNum = len(fileDF.columns)
+        for c in fileDF.columns:
+            colCnt += 1
+            print('{}/{} col: {}/{}'.format(cnt, fNum, colCnt, colNum))
+            pdict = {
+                "column_name": c
+            }
+            #1
+            print('#1')
+            nonEmptyCells = noEmptyDF.select(c).first()[c]
+            pdict["number_non_empty_cells"] = int(nonEmptyCells)
+            print('#1')
+            #2
+            print('#2')
+            emptyCells = emptyDF.select(c).first()[c]
+            pdict["number_empty_cells"] = int(emptyCells)
+            print('#2')
+            #3
+            print('#3')
+            disNum = disNumDF.select(c).first()[c]
+            pdict["number_empty_cells"] = int(disNum)
+            print('#3')
+            
+            outputDicts["columns"].append(pdict)
+        outString = str(outputDicts)
+        finalOut.append(outString)
+    outRDD = sc.parallelize(finalOut)
+    outRDD.saveAsTextFile("test.out")
+
+       
+        
+    # out put format
+    # {
+    #     "dataset_name": "",
+    #     "columns":[
+    #         {
+    #             "column_name": "Category",
+    #             "number_non_empty_cells": 20,
+    #                 ....
+    #         },
+    #         {
+    #             "column_name": "Cohort Year",
+    #             "number_non_empty_cells": 20,
+    #                 ....
+    #         }
+    #     ]
+    #     "semantic_types": [
+    #         {
+    #         "semantic_type": label of the semantic type, e.g.: "Business
+    #         name" (type: string)
+    #         "count": the number of values in the column that belong to
+    #         that semantic type (type: integer)
+    #         },
+    #     ]
+    # }
 
     
