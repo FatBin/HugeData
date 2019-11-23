d3f3133f646c9ee536686c53b186fc64cb54192c
Peifu Yu <peifuyu@10-18-86-54.dynapool.wireless.nyu.edu>
2019-11-23 10:43:02 -0500

    update template

--- project.py
+++ project.py
@@ -21,21 +21,14 @@ if __name__ == "__main__":
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
     #test code
-    # filePath = directory + "/" + fileNames[0] +".tsv.gz"
-    # fileRDD = sc.textFile(filePath).map(lambda x: x.split('\t'))
-    # header = fileRDD.first()
-    # fileRDD = fileRDD.filter(lambda x: x!= header)
-    # fileDF = fileRDD.toDF(header)
-    # disNumDF = fileDF.agg(*(F.countDistinct(col(c)).alias(c) for c in fileDF.columns))
+    # name = fileNames[10]
+    # filePath = directory + "/" + name +".tsv.gz"
+    # fileDF = spark.read.format('csv').options(header='true', inferschema='true', delimiter='\t').load(filePath)
+    # print(str(fileDF.dtypes))
 
     for name in fileNames:
         filePath = directory + "/" + name +".tsv.gz"
-        fileRDD = sc.textFile(filePath).map(lambda x: x.split('\t'))
-        header = fileRDD.first()
-        #rdd doesnt has correct names of keys
-        fileRDD = fileRDD.filter(lambda x: x!= header)
-        #add header
-        fileDF = fileRDD.toDF(header)
+        fileDF = spark.read.format('csv').options(header='true', inferschema='true', delimiter='\t').load(filePath)
         #fileDF.coalesce(1).write.format('json').save('test.json')
         #1 non empty cell
         noEmptyDF = fileDF.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in fileDF.columns]).show()
@@ -45,5 +38,8 @@ if __name__ == "__main__":
         disNumDF = fileDF.agg(*(F.countDistinct(col(c)).alias(c) for c in fileDF.columns))
         #4 most frequent top 5
         
-        
+    #5 Data types
+    fileDF.dtypes
+
+
     
