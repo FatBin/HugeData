858d8c8eb86788ae5ed0b4d23f312b142707c084
Xiaoran Sun <small_reynard@outlook.com>
2019-12-07 12:55:12 -0500

    Update area of study

--- task2.py
+++ task2.py
@@ -88,7 +88,7 @@ schoollevel_list = ['k-1', 'k-2', 'k-3', 'k-4','k-5','k-6','k-7','k-8','k-9','k-
 #area_of_study
 area_of_study_list = ['business', 'health professions', 'law & government', 'science & math',\
                       'architecture','visual art & design', 'engineering', 'film/video', 'hospitality, travel, & tourism',\
-                      'environmental science', 'communications', 'teaching', 'jrotc']
+                      'environmental science', 'communications', 'teaching', 'jrotc', 'zoned', 'animal science']
 #city names dict, which will be loaded in main function
 cityDict = {}
 #city agency list
