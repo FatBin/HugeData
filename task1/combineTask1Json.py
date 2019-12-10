a4fd8fc66b48e4eca718e6c693bf729ca7637cf3
ObserverA <peifugo@gmail.com>
2019-12-10 13:04:54 -0500

    results tool

--- task1/combineTask1Json.py
+++ task1/combineTask1Json.py
@@ -10,11 +10,12 @@ cnt = 0
 for root, dirs, files in list_dirs: 
     fNum = len(files)
     #Combine json files
+    print(fNum)
     with open('./task1.json', 'w') as cjsf:
         for f in files: 
             cnt += 1
-            sys.stdout.write("\rcurrent step: {}/{}".format(cnt,fNum))
-            sys.stdout.flush()
+            # sys.stdout.write("\rcurrent step: {}/{}".format(cnt,fNum))
+            # sys.stdout.flush()
             fp = os.path.join(root, f)
             with open(fp,'r') as jsf:
                 jstr = jsf.read()
